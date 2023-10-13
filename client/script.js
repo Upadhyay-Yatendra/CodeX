cfef9194a174751f24db791f809080b3004be5af
YATENDRA UPADHYAY <97337674+Upadhyay-Yatendra@users.noreply.github.com>
2023-10-13 18:30:06 +0530

    Update script.js
    ADded server URl

--- client/script.js
+++ client/script.js
@@ -26,7 +26,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     // fetch data from the server 
-    const response = await fetch('http://localhost:5000',{
+    const response = await fetch('https://codex-a13i.onrender.com',{
         method : 'POST',
         headers:{
             'Content-type' : 'application/json'
